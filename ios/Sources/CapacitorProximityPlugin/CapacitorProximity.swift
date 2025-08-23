af6bebfbcee7daa2727f31e58fe07d21e57f42ef
maximilien0405 <maximilien.zimmermann@ik.me>
2025-08-23 22:40:08 +0200

    fix(ios): FIxed small scrash

--- ios/Sources/CapacitorProximityPlugin/CapacitorProximity.swift
+++ ios/Sources/CapacitorProximityPlugin/CapacitorProximity.swift
@@ -3,10 +3,17 @@ import UIKit
 
 @objc public class CapacitorProximity: NSObject {
     @objc public func enable() {
-        UIDevice.current.isProximityMonitoringEnabled = true
+        DispatchQueue.main.async {
+            UIDevice.current.isProximityMonitoringEnabled = true
+            if !UIDevice.current.isProximityMonitoringEnabled {
+                print("Proximity sensor not supported on this device")
+            }
+        }
     }
 
     @objc public func disable() {
-        UIDevice.current.isProximityMonitoringEnabled = false
+        DispatchQueue.main.async {
+            UIDevice.current.isProximityMonitoringEnabled = false
+        }
     }
 }
